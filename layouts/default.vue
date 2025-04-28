ec9d21ac565976f0c6f77d5c7b0be87fa8d83754
Qashqiriston <akobirtojiyev3@gmail.com>
2025-04-28 06:26:57 +0500

    First commit for market place

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-red">hello</h1>
+  </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
+
+module.exports = {
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+}
